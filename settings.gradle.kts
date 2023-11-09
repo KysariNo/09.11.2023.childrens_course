b991dfa37931ebb527dd608ecbb13397d3b1e871
Kondratiev Kirillius <ko.ki.ig1991@gmail.com>
2023-11-09 19:15:06 +0300

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "09.11.2023.children's course"
+include(":app")
+ 
